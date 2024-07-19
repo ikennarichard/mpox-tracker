ab27a5f1e3a54d2e5e6a9533cba855fcf9143df0
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-19 11:45:47 +0100

    feat: add react query for data fetch

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: async () => {
+        const response = await fetch(
+          'https://raw.githubusercontent.com/ikennarichard/mpox-flexisaf/main/owid-monkeypox-data.json'
+        );
+        if (!response.ok) {
+          throw new Error('Failed to fetch countries');
+        }
+        return response.json();
+      },
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <CssBaseline/>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
