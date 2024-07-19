0bb7cb9f5deacda07be77b63d543c2a2970019ba
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-19 11:47:13 +0100

    refactor: move exports to single source

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { ChartContainer } from "@mui/x-charts/ChartContainer";
+export {
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  FormControl,
+  TextField,
+  BarChart,
+  ChartContainer,
+};
