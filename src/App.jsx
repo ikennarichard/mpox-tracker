1242a5892a68e60951d31367aef5d05a5275ae18
Ikenna Richard <oguejioforrichard@gmail.com>
2024-08-11 08:49:28 +0100

    feat: add page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Header from './components/header/header'
 import { useQuery } from "@tanstack/react-query";
-import Grid from "@mui/material/Grid";
-import Cards from "./components/cards/Cards";
+import Cards from "./components/cards/Card";
 import Charts from "./components/chart/Charts";
-import CountryInput from "./components/countryInput/CountryInput";
+import { Typography, Box, Grid } from "./components";
+import { useState } from "react";
+import WorldChart from "./components/chart/WorldChart";
 
 export default function App() {
+  const [region, setRegion] = useState("World");
+
   const {
     data: countries = [],
     isLoading,
@@ -13,24 +17,99 @@ export default function App() {
     queryKey: ["countries"],
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100svh"
+      >
+        Loading...
+      </Box>
+    );
 
-  if (error) return <div>{"An error has occurred: " + error.message}</div>;
+  if (error)
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100svh"
+      >
+        {"An error has occurred: " + error.message}
+      </Box>
+    );
+  
+  const totalDeaths = Number(countries[region]["Deaths"]);
 
-  console.log(countries["Nigeria"]["date"]);
+  const totalCases = Number(countries[region]["Cases"]);
+
+  function handleSelectRegion(value) {
+    setRegion(value);
+  }
+
   return (
-    <Grid 
+    <Grid
       container
       direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh' }}
-      gap='0.5rem'
-      >
-      <h1>Monkey Pox Tracker</h1>
-      <Cards />
-      <CountryInput />
-      <Charts />
+      sx={{ minHeight: "100svh" }}
+      gap="1rem"
+    >
+      <Header countries={countries} handleSelectRegion={handleSelectRegion} />
+      
+      <Box component="main" display="grid" gap="0.6rem">
+        <Box
+          component="section"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Typography
+            variant="h2"
+            justifyContent="center"
+            fontSize="2rem"
+            fontWeight={600}
+            textAlign="center"
+            marginBlockStart="1rem"
+          >
+            {region}
+          </Typography>
+          <Typography variant="p" textAlign="center" justifySelf="center">
+            Data as of 10 Jul 2024 5:30 PM EDT
+          </Typography>
+          <Box
+            display="grid"
+            gridTemplateColumns="1fr 1fr"
+            gap="3rem"
+            marginBlockStart="1rem"
+          >
+            <Cards cardHeading="Total Cases" total={totalCases} />
+            <Cards
+              cardHeading="Deaths"
+              textColor="tomato"
+              total={totalDeaths}
+            />
+          </Box>
+
+          {region === "World" ? (
+            <WorldChart
+              dates={countries["globalOverview"]["date"].map((item) =>
+                Number(new Date(item).getMonth() + 1)
+              )}
+              cases={countries["globalOverview"]["new_cases"].map(Number)}
+            />
+          ) : (
+            <Charts
+              totalCases={totalCases}
+              totalDeaths={totalDeaths}
+              region={region}
+            />
+          )}
+        </Box>
+      </Box>
     </Grid>
   );
 }
