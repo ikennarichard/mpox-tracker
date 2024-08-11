f5e5c9f22f5d048c6d590953a87cadea2606d687
Ikenna Richard <oguejioforrichard@gmail.com>
2024-08-11 08:48:13 +0100

    feat: add mui components

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,31 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import CardHeader from "@mui/material/CardHeader";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { ChartContainer } from "@mui/x-charts/ChartContainer";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Grid from "@mui/material/Grid";
+import { LineChart } from "@mui/x-charts/LineChart"
+
 export {
   Card,
   CardContent,
+  LineChart,
   Typography,
   Box,
   FormControl,
   TextField,
   BarChart,
   ChartContainer,
+  InputLabel,
+  MenuItem,
+  Select,
+  CardHeader,
+  Grid
 };
